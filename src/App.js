928220a9e83fb79515e4a6c1752bd97bfc126b4c
lz-junior <correajr80@hotmail.com>
2023-05-30 20:49:28 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Membro from './components/Membro'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Membro nome="Junior"/>
+      </div>
+    )
+  }
+}
+export default App;
