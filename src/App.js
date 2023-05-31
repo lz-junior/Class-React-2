cffe3ad6178a5ca7ca726432cfb25f0a8aad7d61
lz-junior <correajr80@hotmail.com>
2023-05-30 22:19:17 -0300

    class forty ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
-import Membro from './components/Membro'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      status: false
+    }
+    this.sair = this.sair.bind(this);
+    this.entrar = this.entrar.bind(this);
+  }
+
+  sair() {
+    this.setState({status: false});
+  }
+  entrar() {
+    this.setState({status: true});
+  }
+
   render() {
     return (
       <div>
-        <Membro nome="Junior"/>
+        <h2>Curso React</h2>
+        {this.state.status ? 
+          <div>
+            <h2>Bem-vindo ao sistema</h2>
+            <button onClick={this.sair}>Sair</button>
+          </div> :
+          <div>
+            <h2>Olá visitante, faça o login</h2>
+            <button onClick={this.entrar}>Entrar no sistema</button>
+          </div>
+        }
       </div>
     )
   }
 }
-export default App;
+
+export default App
