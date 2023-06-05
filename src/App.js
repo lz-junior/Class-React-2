3ac5e9afffc64d4d9331c63c67ba37bdedd4250d
lz-junior <correajr80@hotmail.com>
2023-06-05 20:26:08 -0300

    class forty eight

--- src/App.js
+++ src/App.js
@@ -1,59 +1,11 @@
-import React, { Component } from 'react'
-import './style.css'
+import React from 'react';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      numero: 0,
-      botao: "INICIAR"
-    };
-    this.timer = null;
-    this.iniciar = this.iniciar.bind(this);
-    this.parar = this.parar.bind(this);
-  }
+function App() {
+  return (
+    <div>
 
-  iniciar() {
-    let state = this.state
-
-    if (this.timer !== null) {
-      clearInterval(this.timer);
-      this.timer = null;
-      state.botao = 'INICIAR'
-    }else{ 
-      this.timer = setInterval(()=> {
-        let state = this.state;
-        state.numero += 0.1;
-        this.setState(state);
-      },100);
-      state.botao = 'PAUSAR';
-    }
-    this.setState(state);
-  }
-
-  parar() {
-    if(this.timer !== null){
-      clearInterval(this.timer);
-      this.timer = null;
-    }
-    let state = this.state;
-    state.numero = 0;
-    state.botao = 'INICIAR';
-    this.setState(state);
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <img src={require('./assets/cronometro.png')} className="img" />
-        <a className="timer">{this.state.numero.toFixed(1)}</a>
-        <div className='areaBtn'>
-          <a className='botao' onClick={this.iniciar}>{this.state.botao}</a>
-          <a className='botao' onClick={this.parar}>PARAR</a>
-        </div>
-      </div>
-    )
-  }
+    </div>
+  );
 }
 
 export default App
