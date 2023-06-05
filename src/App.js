7d2debaf92053b731f88375bcb607c72ec60228f
lz-junior <correajr80@hotmail.com>
2023-06-05 20:50:21 -0300

    adicionando por useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar React Hooks'
+  ]);
+
+  function handleAdd() {
+    setTarefas([...tarefas, 'Aprender JavaScript'])
+  }
+
   return (
     <div>
-
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleAdd}>Adicionar</button>
     </div>
   );
 }
