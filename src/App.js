b8d89706cc1ef9a27b16307e8a44bba94deb3af8
lz-junior <correajr80@hotmail.com>
2023-06-05 21:00:31 -0300

    button add useState

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ function App() {
     'Estudar React Hooks'
   ]);
 
+  const [input, setInput] = useState('');
+
   function handleAdd() {
-    setTarefas([...tarefas, 'Aprender JavaScript'])
+    setTarefas([...tarefas, input]);
+    setInput('');
   }
 
   return (
@@ -18,6 +21,11 @@ function App() {
           <li key={tarefa}>{tarefa}</li>
         ))}
       </ul>
+      <input 
+        type="text"
+        value={input}
+        onChange={e => setInput(e.target.value)}
+      />
       <button type="button" onClick={handleAdd}>Adicionar</button>
     </div>
   );
