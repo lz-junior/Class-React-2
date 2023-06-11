652886fab4ac5862bcad3ad53609936f6241c588
lz-junior <correajr80@hotmail.com>
2023-06-11 14:53:57 -0300

    requisicoes http - jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import './style.css'
 
 //https://sujeitoprogramador.com/rn-api/?api=posts
 
@@ -8,27 +8,39 @@ function App() {
   
   const [nutri, setNutri] = useState([]);
 
+  // Chamando API
   useEffect(()=> {
-
     function loadApi() {
       let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
 
       fetch(url)
       .then((r)=> r.json())
       .then((json)=> {
-        console.log(json)
+        console.log(json);
+        setNutri(json);
       })
-
     }
-
     loadApi()
   }, [])
 
 
 
   return (
-    <div>
-      <h1>Teste</h1>
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
+      </header>
+
+      {nutri.map((item)=> {
+        return (
+          <article key={item.id} className='post'>
+            <strong className="titulo">{item.titulo}</strong>
+            <img className="capa" src={item.capa} alt={item.titulo}/>
+            <p className="subtitulo">{item.subtitulo}</p>
+            <a className="botao">Acessar</a>
+          </article>
+        )
+      })}
     </div>
   )
 }
