d9a537639bd43b41614f2ef0c1aa8376df2f2de8
lz-junior <correajr80@hotmail.com>
2023-06-12 22:35:12 -0300

    routes-pagina de erro

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Contato from './pages/Contato';
+import Erro from './pages/Erro';
 import Header from './components/Header';
 
 function RoutesApp() {
@@ -12,6 +13,8 @@ function RoutesApp() {
         <Route path="/" element={<Home/>}/>
         <Route path="/sobre" element={<Sobre/>}/>
         <Route path="/contato" element={<Contato/>}/>
+
+        <Route path="*" element={<Erro/>}/>
       </Routes>
     </BrowserRouter>
   )
