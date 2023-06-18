c01833cc55e71821cbf3fab57b1b714626141419
lz-junior <correajr80@hotmail.com>
2023-06-18 18:23:42 -0300

    projetc-film page-film requisicaoID

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,11 +1,33 @@
-
+import {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import api from '../../services/api';
 
 function Filme() {
+  const { id } = useParams();
+  
+  useEffect(()=> {
+    async function loadFilme() {
+      await api.get(`/movie/${id}`, {
+        params: {
+          api_key: "4fb839c6ef39db9d421b671118ce07fe",
+          language: "pt-BR",
+        }
+      })
+      .then((response)=> {
+        console.log(response.data);
+      })
+      .catch(()=> {
+        console.log("filme não encontrado")
+      })
+    }
+    loadFilme();
+  }, []);
+
   return (
     <div>
-      <h2>Sinopse do filme</h2>
+      <h2>{id}</h2>
     </div>
   )
-}
+};
 
 export default Filme;
