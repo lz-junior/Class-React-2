7090b43140d89b800dcc1430e57fb2ed96293119
lz-junior <correajr80@hotmail.com>
2023-06-18 18:38:29 -0300

    project-film page-film ok

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -4,7 +4,10 @@ import api from '../../services/api';
 
 function Filme() {
   const { id } = useParams();
-  
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+
   useEffect(()=> {
     async function loadFilme() {
       await api.get(`/movie/${id}`, {
@@ -14,18 +17,37 @@ function Filme() {
         }
       })
       .then((response)=> {
-        console.log(response.data);
+        setFilme(response.data);
+        setLoading(false);
       })
       .catch(()=> {
         console.log("filme não encontrado")
       })
     }
     loadFilme();
+
+    return ()=> {
+      console.log("Componente foi desmontado");
+    };
+
   }, []);
 
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <h2>{id}</h2>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+    
     </div>
   )
 };
