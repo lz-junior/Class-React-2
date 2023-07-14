f559d7a1cf49a1d0df3c74a932bda966a97d2c9f
lz-junior <correajr80@hotmail.com>
2023-07-14 00:04:10 -0300

    ToDo - admin 0.3

--- react-firebase/react-firebase/src/pages/Admin/index.js
+++ react-firebase/react-firebase/src/pages/Admin/index.js
@@ -1,9 +1,29 @@
+import { useState } from 'react';
 import './admin.css';
 
 export default function Admin() {
+  const [tarefaInput, setTarefaInput] = useState('');
+
+
+  function handleRegister(e) {
+    e.preventDefault();
+    alert("Clicou");
+  }
+
+
   return (
-    <div>
-      <h1>Página Admin</h1>
+    <div className="admin-container">
+      <h1>Minhas tarefas</h1>
+
+      <form className="form" onSubmit={handleRegister}>
+        <textarea 
+          placeholder="Digite sua tarefa..."
+          value={tarefaInput}
+          onChange={(e)=> setTarefaInput(e.target.value)}
+        />
+        <button type="submit">Registrar tarefa</button>
+      </form>
+
     </div>
   )
 }
